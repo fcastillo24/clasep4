aa8ffd63a143494f7235e836f7ad80bd5265c8b1
fredy <casfredy21@gmail.com>
2024-02-24 11:33:10 -0600

    Agregar archivos de proyecto.

---
+++ clasep4/Program.cs
@@ -0,0 +1,85 @@
+﻿//    for (int x =1; x <= 10; x++)
+//    {
+//        if (x% 2 == 0)
+//        {
+//            Console.WriteLine(x);
+//        }
+//    }
+
+
+
+//int result = 0;
+//for (int x=0; x<= 10; x++)
+//{
+//    result = result + x;
+//}
+//Console.WriteLine(result);
+
+
+//int x = 0;
+//int y = 0;
+//do
+//{
+//    Console.Write("Ingrese un número entero positivo (ingrese un número negativo para terminar): ");
+//    x = int.Parse(Console.ReadLine());
+//    if (x >= 0)
+//    {
+//        y = x + y;
+//    }
+//} while (x >= 0);
+//Console.WriteLine("La suma de los números ingresados es: " + y);
+
+
+//int x = 1;
+//while (true)
+//{
+//    if (x % 7 == 0 && x % 11 == 0)
+//    {
+//        Console.WriteLine("El primer número entero positivo divisible entre 7 y 11 es: " + x);
+//        break;
+//    }
+//    x++;
+//}
+
+//string contra = "";
+//do
+//{
+//    Console.WriteLine("ingrese contraseña");
+//    contra = Console.ReadLine();
+//    if (contra == "jutiapa")
+//    {
+//        Console.WriteLine("Bienvenido, usurio");
+//        break;
+//    }
+//}
+//while (true);
+
+
+using System.ComponentModel.Design;
+
+//Random random = new Random();
+//int randomNumeroInRange = random.Next(0, 100);
+//int x = 0;
+//do
+//{
+//    Console.WriteLine("ingrese un numero entre 0 y 100");
+//    x=int.Parse(Console.ReadLine());
+//    if (x > randomNumeroInRange)
+//    {
+//        Console.WriteLine("el numero que ingresastes es mayor que el numero aleatoreio");
+//    }
+//    else if 
+//        {
+//        Console.WriteLine(x < randomNumeroInRange)
+//            }
+//    else if (x < randomNumeroInRange)
+//            {
+//            Console.WriteLine("el numero que ingresastes es manor que el numero aleatorio");
+//        }
+//        else if (x == randomNumeroInRange)
+//    {
+//        Console.WriteLine("el numero es correcto");
+//        break;
+//    }
+//}
+//while (true);
